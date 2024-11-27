375220e3716a99b85e2ed3b3e26f8c590232fe4f
Arbsht <shtjefni.arbi@iisgalvanimi.edu.it>
2024-11-27 10:40:02 +0100

    Aggiunte

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+
+lista_spesa = []
 
 #inizializza l'app Flask
 app = Flask(__name__)
@@ -6,7 +8,26 @@ app = Flask(__name__)
 #rotta principale
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', lista_spesa = lista_spesa)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    print(lista_spesa)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
+
+@app.route('/svuota', methods=['POST'])
+def svuota():
+    lista_spesa.clear()
+    return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
